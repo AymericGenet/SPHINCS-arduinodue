e7dda9f160c91fa22c33c2a1e2b2afecabba271f
Genêt Aymeric <aymeric.genet@kudelskisecurity.com>
2017-05-02 11:27:52 +0200

    Add ChaCha12 implementation from ECRYPT to Crypto project.

---
+++ Crypto/src/crypto_chacha12.h
@@ -0,0 +1,19 @@
+/*
+ * crypto_chacha12.h
+ *
+ */ 
+
+#include "defs.h"
+
+#ifndef CRYPTO_CHACHA12_H_
+#define CRYPTO_CHACHA12_H_
+
+#define CHACHA12_KEY_BYTES 32
+#define CHACHA12_NONCE_BYTES 8
+
+void chacha12_perm_c(u32 x[16], const u32 input[16]);
+
+int crypto_chacha12(unsigned char * out, unsigned long long const outlen,
+                    unsigned char const * nonce, unsigned char const * key);
+
+#endif /* CRYPTO_CHACHA12_H_ */
