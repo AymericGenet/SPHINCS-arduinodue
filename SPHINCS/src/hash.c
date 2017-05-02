8cde1fd7974ce4089d3374faab3263c7e983bbf4
Genêt Aymeric <aymeric.genet@kudelskisecurity.com>
2017-05-02 11:29:35 +0200

    Add hash functions to SPHINCS project.

---
+++ SPHINCS/src/hash.c
@@ -0,0 +1,178 @@
+/*
+ * hash.c
+ *
+ */
+
+#include "BLAKE-256.h"
+#include "crypto_chacha12.h"
+#include "parameters.h"
+#include "ecrypt-sync.h"
+
+/* Constant chosen by SPHINCS authors */
+static char const * hash_const_c = "expand 32-byte to 64-byte state!";
+
+int hash_any_n(unsigned char * out, unsigned char const * in,
+               unsigned long long const inlen)
+{
+	crypto_hash_blake256(out, in, inlen);
+	return 0;
+}
+
+int hash_n_n(unsigned char * out, unsigned char const * in)
+{
+	int i;
+	u32 tmp[SPHINCS_BYTES/2];
+
+	/* Translate the input in integers and expand with bytes from literal string */
+	for (i = 0; i < SPHINCS_BYTES/4; ++i)
+	{
+		tmp[i] = U8TO32_LITTLE(in + 4*i);
+		tmp[i + 8] = U8TO32_LITTLE(hash_const_c + 4*i);
+	}
+
+	/* Apply the permutation step of ChaCha12 */
+	chacha12_perm_c(tmp, tmp);
+
+	/* Translate the (SPHINCS_BYTES/4) first integers in bytes */
+	for (i = 0; i < SPHINCS_BYTES/4; ++i)
+	{
+		U32TO8_LITTLE(out + 4*i, tmp[i]);
+	}
+
+	return 0;
+}
+
+int hash_n_n_mask(unsigned char * out, unsigned char const * in,
+                  unsigned char const * mask)
+{
+	int i;
+	unsigned char tmp[SPHINCS_BYTES];
+
+	/* Mask input with XOR operation */
+	for (i = 0; i < SPHINCS_BYTES; ++i)
+	{
+		tmp[i] = in[i] ^ mask[i];
+	}
+
+	return hash_n_n(out, tmp);
+}
+
+int hash_2n_n(unsigned char * out, unsigned char const * in)
+{
+	int i;
+	u32 tmp[SPHINCS_BYTES/2];
+
+	/* Translate the input in integers and expand with bytes from literal string */
+	for(i = 0; i < SPHINCS_BYTES/4; ++i)
+	{
+		tmp[i] = U8TO32_LITTLE(in + 4*i);
+		tmp[i + 8] = U8TO32_LITTLE(hash_const_c + 4*i);
+	}
+
+	/* Apply the permutation step of ChaCha12 */
+	chacha12_perm_c(tmp, tmp);
+
+	/* XOR the result with next SPHINCS_BYTES bytes of the input */
+	for(i = 0; i < SPHINCS_BYTES/4; ++i)
+	{
+		tmp[i] = tmp[i] ^ U8TO32_LITTLE(in + 4*i + SPHINCS_BYTES);
+	}
+
+	/* Re-apply the permutation step of ChaCha12 */
+	chacha12_perm_c(tmp, tmp);
+
+	/* Translate the (SPHINCS_BYTES/4) first integers in bytes */
+	for (i = 0; i < SPHINCS_BYTES/4; ++i)
+	{
+		U32TO8_LITTLE(out + 4*i, tmp[i]);
+	}
+
+	return 0;
+}
+
+int hash_2n_n_mask(unsigned char * out, unsigned char const * in,
+                   unsigned char const * mask)
+{
+	int i;
+	unsigned char tmp[2*SPHINCS_BYTES];
+
+	/* Mask input with XOR operation */
+	for (i = 0; i < 2*SPHINCS_BYTES; ++i)
+	{
+		tmp[i] = in[i] ^ mask[i];
+	}
+
+	return hash_2n_n(out, tmp);
+}
+
+int hash_nn_n(unsigned char * out, unsigned char const * in1,
+              unsigned char const * in2)
+{
+	int i;
+	unsigned char tmp[2*SPHINCS_BYTES];
+
+	/* Concatenate inputs in parallel */
+	for (i = 0; i < SPHINCS_BYTES; ++i)
+	{
+		tmp[i] = in1[i];
+		tmp[i + SPHINCS_BYTES] = in2[i];
+	}
+
+	return hash_2n_n(out, tmp);
+}
+
+int hash_nn_n_mask(unsigned char * out, unsigned char const * in1,
+                   unsigned char const * in2, unsigned char const * mask)
+{
+	int i;
+	unsigned char tmp[2*SPHINCS_BYTES];
+
+	/* Mask input with XOR operation while concatenating inputs in parallel */
+	for (i = 0; i < SPHINCS_BYTES; ++i)
+	{
+		tmp[i] = in1[i] ^ mask[i];
+		tmp[i + SPHINCS_BYTES] = in2[i] ^ mask[i + SPHINCS_BYTES];
+	}
+
+	return hash_2n_n(out, tmp);
+}
+
+int hash_chain_n(unsigned char * out, unsigned char const * in,
+                 unsigned long long const chainlen)
+{
+	unsigned int i;
+
+	/* Initialize chain */
+	for (i = 0; i < SPHINCS_BYTES; ++i)
+	{
+		out[i] = in[i];
+	}
+
+	/* Apply hash function in chain */
+	for (i = 0; i < chainlen; ++i)
+	{
+		hash_n_n(out, out);
+	}
+
+	return 0;
+}
+
+int hash_chain_n_mask(unsigned char * out, unsigned char const * in,
+                      unsigned char * masks, unsigned long long const chainlen)
+{
+	unsigned int i;
+
+	/* Initialize chain */
+	for (i = 0; i < SPHINCS_BYTES; ++i)
+	{
+		out[i] = in[i];
+	}
+
+	/* Apply hash function in chain and mask every step with XOR operation */
+	for (i = 0; i < chainlen; ++i)
+	{
+		hash_n_n_mask(out, out, masks + (i*SPHINCS_BYTES));
+	}
+
+	return 0;
+}
---
+++ SPHINCS/src/hash.h
@@ -0,0 +1,101 @@
+/*
+ * hash.h
+ *
+ */
+
+#ifndef HASH_H_
+#define HASH_H_
+
+#include "parameters.h"
+
+/*
+ * Hashes messages of any bytes to digest of SPHINCS_BYTES bytes.
+ */
+
+int hash_any_n(unsigned char * out, unsigned char const * in,
+               unsigned long long const inlen);
+
+/*
+ * Hashes messages of SPHINCS_BYTES bytes to digest of SPHINCS_BYTES bytes.
+ *
+ * The method specified by the SPHINCS documentation starts by expanding the
+ * initial input with the bytes of the literal ASCII representation of the
+ * string "expand 32-byte to 64-byte state!". Then, it makes use of the
+ * permutation step from the stream cipher ChaCha12 and truncates the
+ * SPHINCS_BYTES first bytes of the result.
+ */
+
+int hash_n_n(unsigned char * out, unsigned char const * in);
+
+/*
+ * Hashes messages of SPHINCS_BYTES bytes to digest of SPHINCS_BYTES bytes, as
+ * it is done with hash_n_n(), but initially XOR the input with the provided
+ * mask.
+ */
+
+int hash_n_n_mask(unsigned char * out, unsigned char const * in,
+                  unsigned char const * mask);
+
+/*
+ * Hashes messages of 2*SPHINCS_BYTES bytes to digest of SPHINCS_BYTES bytes.
+ *
+ * The method specified by the SPHINCS documentation starts by splitting the
+ * input in two: (M1 || M2).
+ * 
+ * The first input M1 is expanded with the bytes of the literal ASCII
+ * representation of the string "expand 32-byte to 64-byte state!", while the
+ * second input M2 is expanded with 32 bytes equal to 0. Then, the method
+ * applies the permutation step from the stream cipher ChaCha12 on the expanded
+ * M1, XOR the output with the the expanded M2, and re-applies the same
+ * permutation step on the result. The method finishes by truncating the
+ * first SPHINCS_BYTES of the outcome from this whole process.
+ */
+
+int hash_2n_n(unsigned char * out, unsigned char const * in);
+
+/*
+ * Hashes messages of 2*SPHINCS_BYTES bytes to digest of SPHINCS_BYTES bytes,
+ * as it is done with hash_2n_n(), but initially XOR the input with the
+ * provided mask.
+ */
+
+int hash_2n_n_mask(unsigned char * out, unsigned char const * in,
+                   unsigned char const * mask);
+
+/*
+ * Hashes the concatenation of two messages of SPHINCS_BYTES bytes, resulting
+ * in a message of 2*SPHINCS_BYTES bytes.
+ *
+ * The method basically concatenates the two inputs in1 and in2 together and
+ * uses hash_2n_n().
+ */
+
+int hash_nn_n(unsigned char * out, unsigned char const * in1,
+              unsigned char const * in2);
+
+/*
+ * Hashes the concatenation of two messages of SPHINCS_BYTES bytes, resulting
+ * in a message of 2*SPHINCS_BYTES bytes, as it is done with hash_nn_n(), but
+ * initially XOR the inputs with the provided mask.
+ */
+
+int hash_nn_n_mask(unsigned char * out, unsigned char const * in1,
+                   unsigned char const * in2, unsigned char const * mask);
+
+/*
+ * Hashes a message of SPHINCS_BYTES bytes in chain (chainlen times) to a
+ * digest of SPHINCS_BYTES bytes.
+ */
+
+int hash_chain_n(unsigned char * out, unsigned char const * in,
+                 unsigned char const * mask, unsigned long long const chainlen);
+
+/*
+ * Hashes a message of SPHINCS_BYTES bytes in chain (chainlen times) to a
+ * digest of SPHINCS_BYTES bytes, but XOR each step with the provided mask.
+ */
+
+int hash_chain_n_mask(unsigned char * out, unsigned char const * in,
+                      unsigned char const * mask, unsigned long long const chainlen);
+
+#endif /* HASH_H_ */
