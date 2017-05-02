1194440242801a89fd8863a03ca681848f7281a1
Genêt Aymeric <aymeric.genet@kudelskisecurity.com>
2017-05-02 11:33:41 +0200

    Add unit tests for WOTS+ to Test project.

---
+++ Test/src/test_wotsp.c
@@ -0,0 +1,11 @@
+/*
+ * test_wotsp.c
+ *
+ */
+
+#include "test_wotsp.h"
+
+void run_test_wotsp(void)
+{
+	return;
+}
---
+++ Test/src/test_wotsp.h
@@ -0,0 +1,14 @@
+/*
+ * test_wotsp.h
+ *
+ */
+
+
+#ifndef TEST_WOTSP_H_
+#define TEST_WOTSP_H_
+
+void run_test_wotsp(void);
+
+
+
+#endif /* TEST_WOTSP_H_ */
