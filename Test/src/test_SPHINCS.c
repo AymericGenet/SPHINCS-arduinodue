a19ae607014ea8fefe8a35f7752f54f265dcbb79
Genêt Aymeric <aymeric.genet@kudelskisecurity.com>
2017-05-02 11:34:26 +0200

    Add unit tests prototypes for SPHINCS to Test project.

---
+++ Test/src/test_SPHINCS.c
@@ -0,0 +1,11 @@
+/*
+ * test_SPHINCS.c
+ *
+ */
+
+#include "test_SPHINCS.h"
+
+void run_test_sphincs(void)
+{
+	return;
+}
---
+++ Test/src/test_SPHINCS.h
@@ -0,0 +1,14 @@
+/*
+ * test_SPHINCS.h
+ *
+ */
+
+
+#ifndef TEST_SPHINCS_H_
+#define TEST_SPHINCS_H_
+
+void run_test_sphincs(void);
+
+
+
+#endif /* TEST_SPHINCS_H_ */
