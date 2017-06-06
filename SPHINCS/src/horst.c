546de0021cafda7308f0394a7f3b6e413ba50944
Genêt Aymeric <aymeric.genet@kudelskisecurity.com>
2017-06-06 11:14:06 +0200

    Add HORST signature procedure with unit test.

--- SPHINCS/src/horst.c
+++ SPHINCS/src/horst.c
@@ -43,6 +43,31 @@ static int horst_leafcalc(struct Node * node, unsigned int leaf)
 	}
 }
 
+static void compute_auth_path_id(unsigned int auth_path_id[HORST_TAU], unsigned int block)
+{
+	int i = 0;
+
+	for (i = 0; i < HORST_TAU; ++i)
+	{
+		if ((block & 1) == 0)
+		{
+			auth_path_id[i] = block + 1;
+		}
+		else
+		{
+			auth_path_id[i] = block - 1;
+		}
+		block /= 2;
+	}
+}
+
+static int uint_cmp(const void * a, const void * b)
+{
+	return (*(unsigned int *)a) < (*(unsigned int *)b) ? -1 : 1 /* true */;
+}
+
+
+
 int horst_keygen(unsigned char const seed[SEED_BYTES],
                  unsigned char const masks[HORST_TAU*SPHINCS_BYTES])
 {
@@ -68,26 +93,78 @@ int horst_keygen(unsigned char const seed[SEED_BYTES],
 	return 0;
 }
 
-int horst_sign(unsigned char const digest[SPHINCS_BYTES],
+int horst_sign(unsigned char const digest[SPHINCS_DIGEST_BYTES],
                unsigned char const seed[SEED_BYTES],
                unsigned char const masks[WOTS_MAX_INT*SPHINCS_BYTES])
 {
-	int i, j;
-	unsigned char x[HORST_T*SPHINCS_BYTES];
-	unsigned int m[HORST_K];
+	unsigned int i = 0, j = 0, k = 0, leaf = 0;
+	ECRYPT_ctx ctx_secret;
+	struct Node node;
+	struct Stack stack;
+	stack.index = -1;
+	unsigned int auth_path_id[HORST_TAU];
+	unsigned char auth_path_hash[HORST_TAU*SPHINCS_BYTES];
+	unsigned int blocks[HORST_K];
 
-	/* Splits digest in HORST_K chunks of HORST_TAU bytes */
+	/* Splits the message digest in HORST_K blocks of HORST_TAU bits */
 	for (i = 0; i < HORST_K; ++i)
 	{
-		SPLIT_16(i, m[i], digest);
+		SPLIT_16(i, blocks[i], digest);
 	}
 
-	
+	/* Sorts the blocks in ascending order */
+	qsort(blocks, HORST_K, sizeof(unsigned int), uint_cmp);
 
-	/* Outputs on the stdout */
-	for (i = 0; i < SPHINCS_BYTES; ++i)
+	/* Outputs the secret values that correspond to the blocks in one PRNG traversal */
+	prng_context_setup(&ctx_secret, seed);
+	for (i = 0; i < HORST_K; ++i)
 	{
-		printf("%02x", x[i]);
+		/* Prepares the treehash */
+		compute_auth_path_id(auth_path_id, blocks[i]);
+		leaf = 0;
+		prng_context_setup(&ctx, seed);
+		horst_leafcalc(&node, leaf++);
+
+		/* Computes the authentication path */
+		for (j = 0; j < 2*(HORST_T - 1); ++j)
+		{
+			leaf = l_treehash_mask(&node, &stack, 1, leaf, horst_leafcalc, masks);
+			if (auth_path_id[node.level] == node.id)
+			{
+				for (k = 0; k < SPHINCS_BYTES; ++k)
+				{
+					auth_path_hash[k + node.level*SPHINCS_BYTES] = node.hash[k];
+				}
+			}
+		}
+
+		/* Offset in-place:
+		 *   if i == 0: draw data until you get to the correct secret value
+		 *   else: draw the difference of data to get to the correct secret value */
+		for (j = 0; j < (i == 0 ? blocks[i]/2 + 1 : blocks[i]/2 - blocks[i-1]/2); ++j)
+		{
+			prng_next(&ctx_secret, tmp);
+		}
+
+		/* Outputs secret value on the stdout */
+		for (j = 0; j < SPHINCS_BYTES; ++j)
+		{
+			printf("%02x", tmp[j + (blocks[i] & 1)*SPHINCS_BYTES]);
+			/* Erases it asap */
+			tmp[j] = 0;
+			tmp[j + SPHINCS_BYTES] = 0;
+		}
+
+		/* Outputs authentication path on the stdout */
+		for (j = 0; j < HORST_TAU; ++j)
+		{
+			for (k = 0; k < SPHINCS_BYTES; ++k)
+			{
+				printf("%02x", auth_path_hash[k + j*SPHINCS_BYTES]);
+			}
+		}
+
+		prng_context_delete(&ctx);
 	}
 
 	return 0;
--- Test/src/test_horst.c
+++ Test/src/test_horst.c
@@ -12,11 +12,20 @@
 
 static char const * test_horst_keygen(void)
 {
-	/* keygen with in_sk and in_masks */
-	printf("\nHORST root (this makes take a few seconds...):\t");
+	/* keygen with in_sk1 and in_masks */
+	printf("(this may take a few seconds)\n");
 	horst_keygen(in_sk1, in_masks);
 
 	return NULL; /* Skip */
+}
+
+static char const * test_horst_sign(void)
+{
+	/* sign with in_sk1, in_masks, and in1_hashed_512 */
+	printf("(this may take a few seconds)\n");
+	horst_sign(in1_hashed_512, in_sk1, in_masks);
+
+	return NULL;
 }
 
 
@@ -25,6 +34,7 @@ void run_test_horst(void)
 	printf("### TEST_HORST ###\n\n");
 
 	mu_run_test(test_horst_keygen, "test_horst_keygen()");
+	mu_run_test(test_horst_sign, "test_horst_sign()");
 
 	printf("\n");
 }
